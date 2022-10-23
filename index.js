640c683e659742f817c77f37a76f03992a7be9ff
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-23 20:24:09 +0600

    server done

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json')
+const news = require('./data/news.json')
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('news api under working....')
+})
+
+// load all the categories
+app.get('/news-categories', (req, res) => {
+    res.send(categories)
+})
+
+// load all the news of a specific category
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const categoryNews = news.filter(n => n.category_id === id);
+        res.send(categoryNews)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+
+
+
+// load a specific news/single news details by id
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews)
+})
+
+
+
+app.listen(port, () => {
+    console.log("serdragon news server is running on port ", port);
+})
